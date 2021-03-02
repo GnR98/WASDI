a22bed1d6abb48f13ad4071153ed70339101a0bb
Cristiano Nattero <c.nattero@fadeout.it>
2021-03-02 18:09:30 +0100

    Updated test + catch exception

--- stats/src/test/java/stats/GoogleSheetsLiveTest.java
+++ stats/src/test/java/stats/GoogleSheetsLiveTest.java
@@ -31,90 +31,115 @@ import wasdi.shared.utils.TypeConversionUtils;
 
 public class GoogleSheetsLiveTest {
 
-	private static Sheets sheetsService;
+	private static Sheets s_oSheetsService;
 
 	// this id can be replaced with your spreadsheet id
 	// otherwise be advised that multiple people may run this test and update the
 	// public spreadsheet
+	
+	//TODO replace with a suitable spreadsheet ID
 	private static final String SPREADSHEET_ID = "1sILuxZUnyl_7-MlNThjt765oWshN3Xs-PPLfqYe4DhI";
+
 
 	@BeforeClass
 	public static void setup() throws GeneralSecurityException, IOException {
-		sheetsService = SheetsServiceUtil.getSheetsService();
+		s_oSheetsService = SheetsServiceUtil.getSheetsService();
 	}
 
 	@Test
 	public void whenWriteSheet_thenReadSheetOk() throws IOException {
-		ValueRange body = new ValueRange().setValues(
+		ValueRange oBody = new ValueRange().setValues(
 				TypeConversionUtils.getListOfListsOfObjects(
-						Arrays.asList(Arrays.asList("Expenses January"),
-						Arrays.asList("books", "30"), Arrays.asList("pens", "10"), Arrays.asList("Expenses February"),
-						Arrays.asList("clothes", "20"), Arrays.asList("shoes", "5"))
-				)
-		);
-		UpdateValuesResponse result = sheetsService.spreadsheets().values().update(SPREADSHEET_ID, "A1", body)
+								Arrays.asList(Arrays.asList("Expenses January"),
+								Arrays.asList("books", "30"),
+								Arrays.asList("pens", "10"),
+								Arrays.asList("Expenses February"),
+								Arrays.asList("clothes", "20"),
+								Arrays.asList("shoes", "5"))
+						)
+				);
+		UpdateValuesResponse oResult = s_oSheetsService.spreadsheets().values()
+				.update(SPREADSHEET_ID, "A1", oBody)
 				.setValueInputOption("RAW").execute();
 
-		List<ValueRange> data = new ArrayList<>();
-		data.add(new ValueRange().setRange("D1").setValues(TypeConversionUtils.getListOfListsOfObjects(Arrays.asList(Arrays.asList("January Total", "=B2+B3")))));
-		data.add(new ValueRange().setRange("D4").setValues(TypeConversionUtils.getListOfListsOfObjects(Arrays.asList(Arrays.asList("February Total", "=B5+B6")))));
+		List<ValueRange> oData = new ArrayList<>();
+		oData.add(new ValueRange().setRange("D1").setValues(
+				TypeConversionUtils.getListOfListsOfObjects(Arrays.asList(Arrays.asList("January Total", "=B2+B3"))))
+			);
+		oData.add(new ValueRange().setRange("D4").setValues(
+				TypeConversionUtils.getListOfListsOfObjects(Arrays.asList(Arrays.asList("February Total", "=B5+B6"))))
+			);
 
-		BatchUpdateValuesRequest batchBody = new BatchUpdateValuesRequest().setValueInputOption("USER_ENTERED")
-				.setData(data);
-		BatchUpdateValuesResponse batchResult = sheetsService.spreadsheets().values()
-				.batchUpdate(SPREADSHEET_ID, batchBody).execute();
+		BatchUpdateValuesRequest oBatchBody = new BatchUpdateValuesRequest()
+				.setValueInputOption("USER_ENTERED")
+				.setData(oData);
+		BatchUpdateValuesResponse oBatchResult = s_oSheetsService.spreadsheets().values()
+				.batchUpdate(SPREADSHEET_ID, oBatchBody).execute();
 
-		List<String> ranges = Arrays.asList("E1", "E4");
-		BatchGetValuesResponse readResult = sheetsService.spreadsheets().values().batchGet(SPREADSHEET_ID)
-				.setRanges(ranges).execute();
+		List<String> asRanges = Arrays.asList("E1", "E4");
+		BatchGetValuesResponse readResult = s_oSheetsService.spreadsheets().values()
+				.batchGet(SPREADSHEET_ID).setRanges(asRanges).execute();
 
-		ValueRange januaryTotal = readResult.getValueRanges().get(0);
-		assertThat(januaryTotal.getValues().get(0).get(0)).isEqualTo("40");
+		ValueRange oJanuaryTotal = readResult.getValueRanges().get(0);
+		assertThat(oJanuaryTotal.getValues().get(0).get(0)).isEqualTo("40");
 
-		ValueRange febTotal = readResult.getValueRanges().get(1);
-		assertThat(febTotal.getValues().get(0).get(0)).isEqualTo("25");
+		ValueRange oFebruaryTotal = readResult.getValueRanges().get(1);
+		assertThat(oFebruaryTotal.getValues().get(0).get(0)).isEqualTo("25");
 
-		ValueRange appendBody = new ValueRange().setValues(TypeConversionUtils.getListOfListsOfObjects(Arrays.asList(Arrays.asList("Total", "=E1+E4"))));
-		
-		AppendValuesResponse appendResult = sheetsService.spreadsheets().values()
-				.append(SPREADSHEET_ID, "A1", appendBody).setValueInputOption("USER_ENTERED")
+		ValueRange oAppendBody = new ValueRange().setValues(TypeConversionUtils.getListOfListsOfObjects(Arrays.asList(Arrays.asList("Total", "=E1+E4"))));
+
+		AppendValuesResponse oAppendResult = s_oSheetsService.spreadsheets().values()
+				.append(SPREADSHEET_ID, "A1", oAppendBody).setValueInputOption("USER_ENTERED")
 				.setInsertDataOption("INSERT_ROWS").setIncludeValuesInResponse(true).execute();
 
-		ValueRange total = appendResult.getUpdates().getUpdatedData();
-		assertThat(total.getValues().get(0).get(1)).isEqualTo("65");
+		ValueRange oTotal = oAppendResult.getUpdates().getUpdatedData();
+		
+		assertThat(oTotal.getValues().get(0).get(1)).isEqualTo("65");
 	}
 
 	@Test
 	public void whenUpdateSpreadSheetTitle_thenOk() throws IOException {
 
-		UpdateSpreadsheetPropertiesRequest updateRequest = new UpdateSpreadsheetPropertiesRequest().setFields("*")
+		UpdateSpreadsheetPropertiesRequest oUpdateRequest = new UpdateSpreadsheetPropertiesRequest().setFields("*")
 				.setProperties(new SpreadsheetProperties().setTitle("Expenses"));
 
-		CopyPasteRequest copyRequest = new CopyPasteRequest()
-				.setSource(new GridRange().setSheetId(0).setStartColumnIndex(0).setEndColumnIndex(2).setStartRowIndex(0)
-						.setEndRowIndex(1))
-				.setDestination(new GridRange().setSheetId(1).setStartColumnIndex(0).setEndColumnIndex(2)
-						.setStartRowIndex(0).setEndRowIndex(1))
+		GridRange oSource = new GridRange()
+				.setSheetId(0)
+				.setStartColumnIndex(0).setEndColumnIndex(2)
+				.setStartRowIndex(0).setEndRowIndex(1);
+		GridRange oDestination = new GridRange()
+				//TODO check appropriateness of sheet ID
+				.setSheetId(1)
+				.setStartColumnIndex(0).setEndColumnIndex(2)
+				.setStartRowIndex(0).setEndRowIndex(1);
+		CopyPasteRequest oCopyRequest = new CopyPasteRequest()
+				.setSource(oSource)
+				.setDestination(oDestination)
 				.setPasteType("PASTE_VALUES");
 
-		List<Request> requests = new ArrayList<>();
+		List<Request> aoRequests = new ArrayList<>();
+		aoRequests.add(new Request().setCopyPaste(oCopyRequest));
+		aoRequests.add(new Request().setUpdateSpreadsheetProperties(oUpdateRequest));
 
-		requests.add(new Request().setCopyPaste(copyRequest));
-		requests.add(new Request().setUpdateSpreadsheetProperties(updateRequest));
+		BatchUpdateSpreadsheetRequest oBody = new BatchUpdateSpreadsheetRequest().setRequests(aoRequests);
 
-		BatchUpdateSpreadsheetRequest body = new BatchUpdateSpreadsheetRequest().setRequests(requests);
+		BatchUpdateSpreadsheetResponse oResponse = null;
+		try {
+			oResponse = s_oSheetsService.spreadsheets().batchUpdate(SPREADSHEET_ID, oBody).execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(oResponse.toPrettyString());
 
-		BatchUpdateSpreadsheetResponse oResponse = sheetsService.spreadsheets().batchUpdate(SPREADSHEET_ID, body).execute();
-		
 	}
 
 	@Test
 	public void whenCreateSpreadSheet_thenIdOk() throws IOException {
-		Spreadsheet spreadSheet = new Spreadsheet()
+		Spreadsheet oSpreadSheet = new Spreadsheet()
 				.setProperties(new SpreadsheetProperties().setTitle("My Spreadsheet"));
-		Spreadsheet result = sheetsService.spreadsheets().create(spreadSheet).execute();
+		Spreadsheet oResult = s_oSheetsService.spreadsheets().create(oSpreadSheet).execute();
 
-		assertThat(result.getSpreadsheetId()).isNotNull();
+		assertThat(oResult.getSpreadsheetId()).isNotNull();
 	}
 
 }
