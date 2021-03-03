ca37d2f30d83b56589574b769073a4463f59a9c7
Cristiano Nattero <c.nattero@fadeout.it>
2021-03-03 18:09:14 +0100

    Improved sheets wrapper (incl. default key file)

--- stats/src/main/java/stats/GoogleAuthorizeUtil.java
+++ stats/src/main/java/stats/GoogleAuthorizeUtil.java
@@ -22,6 +22,7 @@ import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow.Builder;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -29,11 +30,25 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.MemoryDataStoreFactory;
 import com.google.api.services.sheets.v4.SheetsScopes;
 
+import wasdi.shared.utils.Utils;
+
 
 public class GoogleAuthorizeUtil {
+
+	private GoogleAuthorizeUtil() {
+		throw new IllegalStateException("GoogleAuthorizeUtil is just a utility class, do not construct it, please");
+	}
+
 	public static Credential authorize() throws IOException, GeneralSecurityException {
-		Path oWorkingDirectory = Paths.get(System.getProperty("user.dir")); 
-		String sOAuthPath = oWorkingDirectory.resolve("resources").resolve("client_secret.json").toAbsolutePath().normalize().toString();
+		return authorize(null);
+	}
+
+	public static Credential authorize(String sKeyFile) throws IOException, GeneralSecurityException {
+		Path oWorkingDirectory = Paths.get(System.getProperty("user.dir"));
+		if(Utils.isNullOrEmpty(sKeyFile)) {
+			sKeyFile = "client_secret.json";
+		}
+		String sOAuthPath = oWorkingDirectory.resolve("resources").resolve(sKeyFile).toAbsolutePath().normalize().toString();
 		InputStream in = new FileInputStream(sOAuthPath);
 		//InputStream in = GoogleAuthorizeUtil.class.getResourceAsStream("/google-sheets-client-secret.json");
 		//InputStream in = GoogleAuthorizeUtil.class.getResourceAsStream("/My First Project-ca3e6bdef60d.json");
@@ -49,21 +64,22 @@ public class GoogleAuthorizeUtil {
 		GoogleAuthorizationCodeFlow flow = null;
 		Credential credential = null;
 		try {
-			flow = new GoogleAuthorizationCodeFlow.Builder(
+			Builder oBuilder = new GoogleAuthorizationCodeFlow.Builder(
 					oTransport,
 					oJacksonFactory,
 					clientSecrets,
 					scopes
-					).setDataStoreFactory(new MemoryDataStoreFactory())
-					.setAccessType("offline").build();
-			
+					);
+			oBuilder = oBuilder.setDataStoreFactory(new MemoryDataStoreFactory());
+			flow = oBuilder.setAccessType("offline").build();
+
 			credential = new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		
-		
+
+
 
 		return credential;
 	}
--- stats/src/main/java/stats/SheetsServiceUtil.java
+++ stats/src/main/java/stats/SheetsServiceUtil.java
@@ -19,11 +19,19 @@ import com.google.api.services.sheets.v4.Sheets;
 
 
 public class SheetsServiceUtil {
+	
+	private SheetsServiceUtil() {
+		throw new IllegalStateException("SheetsServiceUtil is just a utility class, do not construct it, please");
+	}
 
     private static final String APPLICATION_NAME = "Google Sheets Example";
 
     public static Sheets getSheetsService() throws IOException, GeneralSecurityException {
-        Credential credential = GoogleAuthorizeUtil.authorize();
+    	return getSheetsService(null);
+    }
+    
+    public static Sheets getSheetsService(String sKeyFile) throws IOException, GeneralSecurityException {
+        Credential credential = GoogleAuthorizeUtil.authorize(sKeyFile);
         return new Sheets.Builder(GoogleNetHttpTransport.newTrustedTransport(), JacksonFactory.getDefaultInstance(), credential).setApplicationName(APPLICATION_NAME).build();
     }
 
